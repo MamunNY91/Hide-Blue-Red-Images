c5f2d0b9515357ab6d69b6aa4c546b8b08b83082
Abdullah A Mamun <abdullahamamun@abdullahs-macbook-pro.local>
2016-01-10 19:51:37 -0500

    created outlet and action

--- Hide Blue Red/ViewController.swift
+++ Hide Blue Red/ViewController.swift
@@ -10,6 +10,12 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var red: UIImageView!
+    @IBOutlet weak var blue: UIImageView!
+    
+    @IBOutlet weak var hideBlue: UIButton!
+    
+    @IBOutlet weak var hideRed: UIButton!
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
@@ -20,6 +26,17 @@ class ViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
+    @IBAction func hideBlueB(sender: AnyObject)
+    {
+        blue.hidden = true
+        
+    }
+    
+    
+    @IBAction func hideRedB(sender: AnyObject)
+    {
+        red.hidden = true
+    }
 
 }
 
